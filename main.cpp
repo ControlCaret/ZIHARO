fb98c265362c58e11b708ff1988d7b7a6782e9f4
Heejae Kwak <controlcaret@gmail.com>
2022-10-31 15:33:46 +0900

    Implement random monster encounter

--- main.cpp
+++ main.cpp
@@ -11,9 +11,11 @@ int main()
     std::cout << "여기에 게임 이름을 입력해 주세요." << std::endl;
     std::cout << "========================" << std::endl;
 
-    std::cout << "1. 새 게임\t 2. 불러오기\t 3. 종료\n" << std::endl;
+    //std::cout << "1. 새 게임\t 2. 불러오기\t 3. 종료\n" << std::endl;
 
     // implement here
+
+    std::cout << std::endl;
 
     struct Player player;
 
@@ -22,7 +24,10 @@ int main()
     player.health = 100;
     player.damage = 10;
     player.level = 1;
-    player.floor = 0;
+    player.floor = 1;
+    srand(time(NULL));
+
+    showPlayerStatus(player);
 
     while(true)
     {
@@ -30,17 +35,35 @@ int main()
 
         Monster monster;
 
-        if(player.floor % 2)
+        // should implement enum
+        int randomMonster = rand() % 10;
+        if(randomMonster == 0)
             monster = slime;
-        else    
+        else if(randomMonster == 1)
             monster = worm;
+        else if(randomMonster == 2)
+            monster = mole;
+        else if(randomMonster == 3)
+            monster = spider;
+        else if(randomMonster == 4)
+            monster = roach;
+        else if(randomMonster == 5)
+            monster = zombie;
+        else if(randomMonster == 6)
+            monster = skeleton;
+        else if(randomMonster == 7)
+            monster = ghost;
+        else if(randomMonster == 8)
+            monster = goblin;
+        else if(randomMonster == 9)
+            monster = racoon;
 
         std::cout << monster.name << "이(가) 나타났다!" << std::endl;
         std::cout << monster.health << " " << monster.damage << std::endl;
 
         while(true)
         {
-            std::cout << "1. 공격" << std::endl;
+            std::cout << "1. 공격\t2. 플레이어 정보\t 3. 몬스터 정보" << std::endl;
 
             int input;
 
@@ -68,6 +91,14 @@ int main()
                     std::cout << "당신은 죽었습니다." << std::endl;
                     return 0;
                 }
+            }
+            if(input == 2)
+            {
+                showPlayerStatus(player);
+            }
+            if(input == 3)
+            {
+                showMonsterStatus(monster);
             }
         }
     }
